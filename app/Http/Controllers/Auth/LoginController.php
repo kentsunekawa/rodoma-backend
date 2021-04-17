5138467297b5aca7dd1e2348983a0183f5175264
Ken Tsunekawa <tsunekawaken@gmail.com>
2021-04-17 23:18:41 +0900

    done

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Auth\AuthManager;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -41,7 +42,7 @@ class LoginController extends Controller
     public function __construct(AuthManager $authManager)
     {
         $this->authManager = $authManager;
-        $this->middleware('guest')->except('logout');
+        // $this->middleware('guest')->except('logout');
     }
 
     public function login(Request $request): JsonResponse
@@ -61,14 +62,49 @@ class LoginController extends Controller
             ], 401);
         }
 
-        $user = User::where('email', $email)->first();
+        return $this->respondWithToken($token);
+    }
 
+    public function refresh() {
+        try {
+            return $this->respondWithToken(auth()->refresh());
+        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
+            return new JsonResponse([
+                'status' => 'fail_refresh_token',
+            ], 401);
+        }
+    }
+
+    protected function respondWithToken($token) {
+        $user = auth()->user();
+        if($user) {
+            $user = [
+                'id' => $user['id'],
+                'name' => $user['name'],
+                'icon_url' => $user['icon_url'],
+            ];
+        }
         return new JsonResponse([
             'status' => 'success_signin',
             'data' => [
                 'user' => $user,
                 'token' => $token,
+                'expires_in' => auth()->factory()->getTTL() * 60,
             ],
         ]);
+    }
+
+    public function getUserByToken() {
+        $user = auth()->user();
+        return new JsonResponse([
+            'status' => 'success_signin',
+            'data' => [
+                'user' => [
+                    'id' => $user['id'],
+                    'name' => $user['name'],
+                    'icon_url' => $user['icon_url'],
+                ],
+            ],
+        ], 200);
     }
 }
