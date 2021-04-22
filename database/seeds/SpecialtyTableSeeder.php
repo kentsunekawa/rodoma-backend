12fc5149c0d6bdf2936f5a1b78f452321a1ee845
Ken Tsunekawa <tsunekawaken@gmail.com>
2021-04-22 23:54:16 +0900

    カテゴリ設定

--- database/seeds/SpecialtyTableSeeder.php
+++ database/seeds/SpecialtyTableSeeder.php
@@ -15,107 +15,263 @@ class SpecialtyTableSeeder extends Seeder
         $specialties = [
             [
                 'category_id' =>  1,
-                'name' => 'specialty1'
+                'name' => '設定なし'
             ],
             [
                 'category_id' =>  2,
-                'name' => 'specialty2'
+                'name' => 'プログラミング言語'
             ],
             [
                 'category_id' =>  2,
-                'name' => 'specialty3'
+                'name' => 'ネットワーク・セキュリティー'
             ],
             [
                 'category_id' =>  2,
-                'name' => 'specialty4'
+                'name' => 'データベース'
             ],
             [
                 'category_id' =>  2,
-                'name' => 'specialty5'
+                'name' => 'AI・人工知能'
             ],
             [
                 'category_id' =>  2,
-                'name' => 'specialty6'
+                'name' => 'OS'
+            ],
+            [
+                'category_id' =>  2,
+                'name' => 'ソフトウェア'
+            ],
+            [
+                'category_id' =>  2,
+                'name' => 'ハードウェア'
+            ],
+            [
+                'category_id' =>  2,
+                'name' => 'IT資格'
+            ],
+            [
+                'category_id' =>  2,
+                'name' => 'その他'
             ],
             [
                 'category_id' =>  3,
-                'name' => 'specialty7'
+                'name' => 'グラフィックデザイン'
             ],
             [
                 'category_id' =>  3,
-                'name' => 'specialty8'
+                'name' => 'WEBデザイン'
             ],
             [
                 'category_id' =>  3,
-                'name' => 'specialty9'
+                'name' => 'ゲームデザイン'
             ],
             [
                 'category_id' =>  3,
-                'name' => 'specialty10'
+                'name' => 'イラストレーション'
             ],
             [
                 'category_id' =>  3,
-                'name' => 'specialty11'
+                'name' => '建築'
+            ],
+            [
+                'category_id' =>  3,
+                'name' => 'ファッション'
+            ],
+            [
+                'category_id' =>  3,
+                'name' => 'インテリア'
+            ],
+            [
+                'category_id' =>  3,
+                'name' => '写真'
+            ],
+            [
+                'category_id' =>  3,
+                'name' => '絵画'
+            ],
+            [
+                'category_id' =>  3,
+                'name' => '音楽'
+            ],
+            [
+                'category_id' =>  3,
+                'name' => '動画'
+            ],
+            [
+                'category_id' =>  3,
+                'name' => '3D・アニメーション'
+            ],
+            [
+                'category_id' =>  3,
+                'name' => '映画'
+            ],
+            [
+                'category_id' =>  3,
+                'name' => 'その他'
             ],
             [
                 'category_id' =>  4,
-                'name' => 'specialty12'
+                'name' => '投資・株・不動産'
             ],
             [
                 'category_id' =>  4,
-                'name' => 'specialty13'
+                'name' => '仮想通貨・ブロックチェーン'
             ],
             [
                 'category_id' =>  4,
-                'name' => 'specialty14'
+                'name' => '貯金・保険'
             ],
             [
                 'category_id' =>  4,
-                'name' => 'specialty15'
+                'name' => '税金・年金'
             ],
             [
                 'category_id' =>  4,
-                'name' => 'specialty16'
+                'name' => '簿記・FP'
+            ],
+            [
+                'category_id' =>  4,
+                'name' => 'その他'
             ],
             [
                 'category_id' =>  5,
-                'name' => 'specialty17'
+                'name' => '英語'
             ],
             [
                 'category_id' =>  5,
-                'name' => 'specialty18'
+                'name' => '中国語'
             ],
             [
                 'category_id' =>  5,
-                'name' => 'specialty19'
+                'name' => '韓国語'
             ],
             [
                 'category_id' =>  5,
-                'name' => 'specialty20'
+                'name' => 'フランス語'
             ],
             [
                 'category_id' =>  5,
-                'name' => 'specialty21'
+                'name' => 'スペイン語'
+            ],
+            [
+                'category_id' =>  5,
+                'name' => 'その他'
             ],
             [
                 'category_id' =>  6,
-                'name' => 'specialty22'
+                'name' => 'ビジネスコミュニケーション'
             ],
             [
                 'category_id' =>  6,
-                'name' => 'specialty23'
+                'name' => 'チーム・マネジメント'
             ],
             [
                 'category_id' =>  6,
-                'name' => 'specialty24'
+                'name' => 'ビジネス戦略'
             ],
             [
                 'category_id' =>  6,
-                'name' => 'specialty25'
+                'name' => 'プロジェクト管理'
             ],
             [
                 'category_id' =>  6,
-                'name' => 'specialty26'
+                'name' => '営業・販売'
+            ],
+            [
+                'category_id' =>  6,
+                'name' => 'ビジネスマナー'
+            ],
+            [
+                'category_id' =>  6,
+                'name' => 'その他'
+            ],
+            [
+                'category_id' =>  7,
+                'name' => 'デジタルマーケティング'
+            ],
+            [
+                'category_id' =>  7,
+                'name' => 'SEO'
+            ],
+            [
+                'category_id' =>  7,
+                'name' => 'SNSマーケティング'
+            ],
+            [
+                'category_id' =>  7,
+                'name' => 'ブランディング'
+            ],
+            [
+                'category_id' =>  7,
+                'name' => 'PR・広告'
+            ],
+            [
+                'category_id' =>  7,
+                'name' => 'コンテンツマーケティング'
+            ],
+            [
+                'category_id' =>  7,
+                'name' => 'マスマーケティング'
+            ],
+            [
+                'category_id' =>  7,
+                'name' => 'グロースハック'
+            ],
+            [
+                'category_id' =>  7,
+                'name' => 'CRM'
+            ],
+            [
+                'category_id' =>  7,
+                'name' => 'ライティング・編集'
+            ],
+            [
+                'category_id' =>  7,
+                'name' => 'その他'
+            ],
+            [
+                'category_id' =>  8,
+                'name' => '筋トレ・ストレッチ'
+            ],
+            [
+                'category_id' =>  8,
+                'name' => 'スポーツ・エクササイズ'
+            ],
+            [
+                'category_id' =>  8,
+                'name' => '美容'
+            ],
+            [
+                'category_id' =>  8,
+                'name' => '食事・料理'
+            ],
+            [
+                'category_id' =>  8,
+                'name' => 'メンタルヘルス'
+            ],
+            [
+                'category_id' =>  8,
+                'name' => '睡眠・休息'
+            ],
+            [
+                'category_id' =>  8,
+                'name' => 'その他'
+            ],
+            [
+                'category_id' =>  9,
+                'name' => '観光'
+            ],
+            [
+                'category_id' =>  9,
+                'name' => '育児'
+            ],
+            [
+                'category_id' =>  9,
+                'name' => '工作・DIY'
+            ],
+            [
+                'category_id' =>  9,
+                'name' => 'その他'
             ],
         ];
         foreach($specialties as $specialty) {
