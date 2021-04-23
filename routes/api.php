c09e42d96072c5dc6f997a1366b53315f7e84a43
Ken Tsunekawa <tsunekawaken@gmail.com>
2021-04-23 22:58:01 +0900

    ルートや許可ドメインの修正

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,9 @@ Route::group(['middleware' => 'api'], function() {
     Route::post('/password/reset/{token}', 'Auth\ResetPasswordController@reset');
     Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
     Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
-
     Route::get('/category_tree', 'AppController@categoryTree');
     Route::get('/sns', 'AppController@sns');
     Route::post('/images', 'ImageController@upload');
-
     Route::resource('/users', 'UserController', ['except' => ['create', 'edit', 'update']]);
     Route::get('/users/{userId}/posts', 'PostController@postsCreatedBySpecificUser');
     Route::get('/users/{userId}/posts/all', 'PostController@allPostsCreatedBySpecificUser');
@@ -34,7 +32,6 @@ Route::group(['middleware' => 'api'], function() {
     Route::get('/users/{userId}/relations/followers', 'RelationController@followers');
     Route::get('/users/{userId}/relations/followers/{followerUserId}', 'RelationController@show');
     Route::put('/users/{userId}/relations/followers/{followerUserId}', 'RelationController@update');
-
     Route::resource('/posts', 'PostController', ['except' => ['create', 'edit']]);
     Route::put('/posts/{postId}/eye_catch', 'PostController@updateEyeCatch');
     Route::get('/posts/{postId}/comments', 'CommentController@commentsByPostId');
@@ -44,8 +41,6 @@ Route::group(['middleware' => 'api'], function() {
     Route::get('/posts/{postId}/marks/{userId}', 'MarkController@show');
     Route::put('/posts/{postId}/likes/{userId}', 'LikeController@update');
     Route::get('/posts/{postId}/likes/{userId}', 'LikeController@show');
-
-
     Route::group(['middleware' => ['jwt.auth']], function() {
         Route::put('/users/{userId}', 'UserController@update');
         Route::put('/users/{userId}/icon', 'UserController@updateIcon');
