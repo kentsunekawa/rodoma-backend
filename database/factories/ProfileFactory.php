8a948acc9065fe1af15bfc439cc89c5fac344a36
Ken Tsunekawa <tsunekawaken@gmail.com>
2021-05-02 22:24:20 +0900

    seeder 作成

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -6,13 +6,48 @@ use App\Models\Profile;
 use Faker\Generator as Faker;
 
 $factory->define(Profile::class, function (Faker $faker) {
+
+    $category_id = $faker->numberBetween(1, 9);
+    $specialty_id;
+
+    switch($category_id) {
+        case 1:
+            $specialty_id = 1;
+            break;
+        case 2:
+            $specialty_id = $faker->numberBetween(2, 10);
+            break;
+        case 3:
+            $specialty_id = $faker->numberBetween(11, 24);
+            break;
+        case 4:
+            $specialty_id = $faker->numberBetween(25, 30);
+            break;
+        case 5:
+            $specialty_id = $faker->numberBetween(31, 36);
+            break;
+        case 6:
+            $specialty_id = $faker->numberBetween(37, 43);
+            break;
+        case 7:
+            $specialty_id = $faker->numberBetween(44, 54);
+            break;
+        case 8:
+            $specialty_id = $faker->numberBetween(55, 61);
+            break;
+        case 9:
+            $specialty_id = $faker->numberBetween(62, 65);
+            break;
+    }
+
+
     return [
         'user_id' => function() {
             return factory(App\Models\User::class)->create()->id;
         },
-        'category_id' => 6,
-        'specialty_id' => $faker->numberBetween(22, 26),
-        'catch_copy' => 'キャッチコピー',
-        'description' => '自己紹介文',
+        'category_id' => $category_id,
+        'specialty_id' => $specialty_id,
+        'catch_copy' => $faker->sentence(rand(2,3)),
+        'description' => 'これはダミーユーザーのデータです。データが登録されたユーザーは[こちら](/users/1)',
     ];
 });
