1f9f09520354d4e36f11adcfebf6aa8e677038fe
Ken Tsunekawa <tsunekawaken@gmail.com>
2021-05-03 23:44:40 +0900

    データ登録

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Comment::class, function (Faker $faker) {
     return [
         'user_id' => $faker->numberBetween(1, 5),
-        'post_id' => 1,
+        'post_id' => 3,
         'comment' => '[dummy message] ' . $faker->sentence(rand(3,10)),
         'created_at' => $faker->dateTimeBetween('-10 day', '-1 day'),
         'updated_at' => $faker->dateTimeBetween('-10 day', '-1 day'),
